720a8b237532fcdac5f81430349e25a538a78a0b
shivaay <shivendrapratap18@gmail.com>
2021-03-24 20:24:31 +0530

    datatype javascript
    Signed-off-by: shivaay <shivendrapratap18@gmail.com>

---
+++ javascript/datatype.js
@@ -0,0 +1,13 @@
+console.log(12354, typeof 12354)
+console.log(5487.256, typeof 5487.256)
+console.log('a', typeof 'a')
+console.log('ggggg', typeof 'ggggg' )
+console.log(true, typeof true)
+console.log(null, typeof null)
+console.log(undefined,typeof undefined)
+console.log(a(), typeof a())
+console.log(a, typeof a)
+
+function a() {
+    return 1
+}
