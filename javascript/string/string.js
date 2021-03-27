e3e1b72e1a1498a6f39d6cea0941f40afc9c97cc
shivaay <shivendrapratap18@gmail.com>
2021-03-27 15:39:11 +0530

    string litrels
    Signed-off-by: shivaay <shivendrapratap18@gmail.com>

---
+++ javascript/string/string.js
@@ -0,0 +1,13 @@
+//he said, "this is awesome!"
+let s1 = 'he said,"this is awesome!'
+let s2 = "he said, this is awesome!//"
+let s3 = "he said, this is awesome///'"
+let s4 = "this is string \n with multiple line"
+let s5 = `
+    this string also 
+    have multiple line
+`
+let name ='Shivendra'
+let g1 =`Good Morning `+ name
+let g2 =`Good Morining ${name}`
+let s7 = `10+2 = ${10+2}`
