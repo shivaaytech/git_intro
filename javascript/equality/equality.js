bf88c0cd0fbdbf0d9701b40f818e2c6d37a3fffd
shivaay <shivendrapratap18@gmail.com>
2021-03-27 16:11:37 +0530

    equality
    Signed-off-by: shivaay <shivendrapratap18@gmail.com>

---
+++ javascript/equality/equality.js
@@ -0,0 +1,17 @@
+console.log(1=='1')
+console.log(1==='1')
+
+console.log(0=='')
+
+console.log('false'==false)
+
+console.log(''==false)
+
+/*'\n'==0 true
+'\t'==0
+''==0
+'\n'=='\t'
+this does not hold good for abstact equality
+a=b,b=c implies that ...a=c
+*/
+console.log([1,2]==[1,2])
